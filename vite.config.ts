620c7d07bd7e820661b5f6ebc0b90df378a4e572
Alexander Schistek <alex.schistek@hotmail.com>
2026-09-07 00:44:59 +0200

    Dev server configuration updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: { port: 5173, strictPort: true },
   build: { target: 'es2022', sourcemap: true },
   test: {
     environment: 'node',
